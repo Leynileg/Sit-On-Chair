cff7fdbc585e97ddf037301c6e93bf4f386c3033
Leynileg <PawelTur123@wp.pl>
2017-11-05 14:11:55 +0100

    update with boostrap

--- js/app.js
+++ js/app.js
@@ -1,59 +1,11 @@
 $(function() {
     
-    //----------- Menu navigation -----------
-    
-    let $otherNav = $('.list').find('li');
-    let $navList = $('.list').find('li')[0];
-    let $aboutList = $('.about_company');
-    
-    $($navList).mouseover(() => {
-         $aboutList.show();
-    });
-    
-    $($navList).mouseout(() => {
-        $aboutList.hide();
-    });
-    
     //----------- Slider -----------
+
+    $('.slide:gt(0)').hide();
     
-    let $leftBtn = $('.switch_left');
-    let $rightBtn = $('.switch_right');
-    let $slider_photos = $('.main_chair').find('li');
-    let counter = 0;
-    $($slider_photos[counter]).css('display', 'inline-block');
+    setInterval(function() {
+      $('.slide:first').fadeOut('slow').next().fadeIn('slow').end().appendTo('.main_chair');
+    }, 5000)
     
-    $rightBtn.on('click', () => {
-        counter++;
-        $($slider_photos[counter-1]).fadeOut(0, function() {
-            $($slider_photos[counter]).fadeIn(500);
-            if (counter >= $slider_photos.length) {
-                counter = 0;
-                $($slider_photos[counter]).fadeIn(500);
-            }
-        });    
-    })
-    
-    $leftBtn.on('click', () => {
-        counter--;
-        $($slider_photos[counter+1]).fadeOut(0, function(){
-            $($slider_photos[counter]).fadeIn(500);
-            
-            if (counter < 0) {
-                counter = $slider_photos.length - 1;
-                $($slider_photos[counter]).fadeIn(500);
-            }
-        });
-    })
-    
-    //----------- Gallery -----------
-    
-    let $photos = $('.gallery_section_photos');
-    
-    $photos.mouseover(function() {
-        $(this).children().fadeOut(500, function(){
-            $photos.mouseout(function() {
-                $(this).children().fadeIn(500);
-            })
-        });
-    });
 });
